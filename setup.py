d7c117314f423174f3e13a06c3b89acbaa481364
Viktor Pec <viktor.pec@fzu.cz>
2024-06-28 12:10:48 +0200

    Adding larfd into the global duneggd setup.

--- setup.py
+++ setup.py
@@ -9,9 +9,8 @@ setup(name = 'duneggd',
       license = 'GPLv2',
       url = 'https://github.com/tyleralion/duneggd',
       package_dir = {'':'python'},
-      packages = ['duneggd', 'duneggd.fgt', 'duneggd.larnd'],
+      packages = ['duneggd', 'duneggd.fgt', 'duneggd.larnd', 'duneggd.larfd'],
       install_requires = [l for l in open("requirements.txt").readlines() if l.strip()],
       # implicitly depends on ROOT
-              
-  )
 
+  )
